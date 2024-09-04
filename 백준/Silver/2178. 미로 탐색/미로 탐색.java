9d6b278d84249d25fae48d790a306fe5cfa25f17
Doocheol <encjf2070@gmail.com>
2024-09-04 09:15:40 +0900

    [Silver I] Title: 미로 탐색, Time: 156 ms, Memory: 16268 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,62 +1,69 @@
+
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int [][] arr;
-	static boolean [][] visit;
-	static int dr [] = {-1,1,0,0};
-	static int dc [] = {0,0,-1,1};
-	static int cnt;
-	public static void main(String[] args) throws Exception{
+	
+	static int N, M;
+	static int[][] adjMatrix, visited;
+	static int[] dr = {-1, 1, 0, 0};
+	static int[] dc = {0, 0, -1, 1};
+	static Deque<int[]> queue = new ArrayDeque<>();
+	
+	public static void main(String[] args) throws Exception {
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String str [] = br.readLine().trim().split(" ");
-		int n = Integer.parseInt(str[0]);
-		int m = Integer.parseInt(str[1]);
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
 		
-		cnt = 0;
-		arr = new int[n+2][m+2];
-		visit = new boolean[n+2][m+2]; 
-		for(int i = 1;i<n+1;i++) {
-			String strarr = br.readLine();
-			for(int j = 1;j<m+1;j++) {
-				arr[i][j] = strarr.charAt(j-1) - '0';
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		adjMatrix = new int[N][M];
+		visited = new int[N][M];
+		
+		for (int row = 0; row < N; row++) {
+			String[] line = br.readLine().split("");
+			for (int column = 0; column < M; column++) {
+				adjMatrix[row][column] = Integer.parseInt(line[column]);
+				visited[row][column] = -1;
 			}
 		}
 		
-		bfs(1,1);
+		bfs(0, 0);
+		sb.append(visited[N-1][M-1]);
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
 		
-		System.out.println(arr[n][m]);
-//		for(int arr1[] : arr) {
-//			for(int x : arr1) {
-//				System.out.print(x + " ");
-//			}
-//			System.out.println();
-//		}
-//	}
 	}
-	private static void bfs(int i, int j) {
-		Queue<int[]> q = new LinkedList<>();
-		q.add(new int[] {i,j});
-		visit[i][j] = true;
-		while(!q.isEmpty()) {
-			int [] now = q.poll();
-			int r = now[0];
-			int c = now[1];
-			for(int k = 0;k<4;k++) {
-				int nr = r+ dr[k];
-				int nc = c+ dc[k];
+
+	private static void bfs(int x, int y) {
+		queue.offer(new int[] {x, y});
+		visited[x][y] = 1; // true 표시
+		
+		while(!queue.isEmpty()) {
+			int[] currentXY = queue.poll();
+			for (int d = 0; d < 4; d++) {
+				int nr = currentXY[0] + dr[d];
+				int nc = currentXY[1] + dc[d];
 				
-				if(arr[nr][nc] == 0 || visit[nr][nc] ) {
-					continue;
+				if (!(nr < 0 || nr >= N || nc < 0 || nc >= M)) {
+					if (adjMatrix[nr][nc] == 1 && visited[nr][nc] == -1) {
+						queue.offer(new int[] {nr, nc});
+						visited[nr][nc] = visited[currentXY[0]][currentXY[1]] + 1;
+					}
 				}
-				arr[nr][nc] = arr[r][c]+1;
-				visit[nr][nc] = true;
-				q.add(new int[] {nr,nc});
 			}
-			
 		}
-		}
+		
+		
 	}
+
+}
